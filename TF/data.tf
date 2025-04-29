d8b9119d14824b8fbf2d91f4c5a94518c9201ffb
Harry Marvell <113520169+HMarvell@users.noreply.github.com>
2025-04-29 20:26:18 +0100

    Update to Config Files

--- TF/data.tf
+++ TF/data.tf
@@ -1,52 +1,21 @@
-data "template_cloudinit_config" "user_scripts_ES" {
-
-  part {
-    content_type = "text/x-shellscript"
-    content = templatefile(
-      "${path.module}/scripts/ES-install.sh",
-      {}
-    )
-  }
-  part {
-    content_type = "text/x-shellscript"
-    content = templatefile(
-      "${path.module}/scripts/elastic-config.sh",
-      {}
-    )
-  }
-  part {
-    content_type = "text/x-shellscript"
-    content = templatefile(
-      "${path.module}/scripts/kibana-config.sh",
-      {
-        PUBLIC_IP = "$(curl http://checkip.amazonaws.com)"
-      }
-    )
-  }
-  part {
-    content_type = "text/x-shellscript"
-    content = templatefile(
-      "${path.module}/scripts/filebeat-config.sh",
-      {}
-    )
-  }
+data "template_file" "user_scripts_es" {
+  template = join("\n",[
+      file("${path.module}/scripts/ES-install.sh"),
+      file("${path.module}/scripts/elastic-config.sh"),
+      file("${path.module}/scripts/filebeat-config.sh")
+    ])
 }
 
-data "template_cloudinit_config" "user_scripts_LS" {
+data "template_file" "user_script_kibana" {
+      template = file("${path.module}/scripts/kibana-config.sh")
+      vars = {
+        PUBLIC_IP = "$(curl http://checkip.amazonaws.com)"
+      }
+}
 
-  part {
-    content_type = "text/x-shellscript"
-    content = templatefile(
-      "${path.module}/scripts/logstash-install.sh",
-      {}
-    )
-  }
-  part {
-    content_type = "text/x-shellscript"
-    content = templatefile(
-      "${path.module}/scripts/logstash-config.sh",
-      {}
-    )
-  }
-
+data "template_file" "user_scripts_ls" {
+  template = join("\n", [
+      file("${path.module}/scripts/logstash-install.sh"),
+      file("${path.module}/scripts/logstash-config.sh")
+  ])
 }
--- TF/main.tf
+++ TF/main.tf
@@ -3,7 +3,9 @@ resource "aws_instance" "TF-ES" {
   ami             = "ami-0c9978668f8d55984"
   instance_type   = "t3.medium"
   key_name        = "HWC-3"
-  user_data       = data.template_cloudinit_config.user_scripts_ES.rendered
+  user_data       = join("\n", [ 
+    data.template_file.user_scripts_es.rendered,
+    data.template_file.user_script_kibana.rendered ])
   security_groups = [aws_security_group.TF_security_group.name]
 
   tags = {
@@ -16,7 +18,7 @@ resource "aws_instance" "TF-LS" {
   ami             = "ami-0c9978668f8d55984"
   instance_type   = "t3.medium"
   key_name        = "HWC-3"
-  user_data       = data.template_cloudinit_config.user_scripts_LS.rendered
+  user_data       = data.template_file.user_scripts_ls.rendered
   security_groups = [aws_security_group.TF_security_group.name]
 
   tags = {
